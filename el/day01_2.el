4ec1bda89e9e53e3ea3e121340fa91fc1662060e
Curtis Hollibaugh <curtis.hollibaugh@protonmail.ch>
2022-12-02 17:17:37 -0800

    Solved day 1 puzzles.

---
+++ el/day01_2.el
@@ -0,0 +1,11 @@
+#!emacs --script
+
+(package-initialize)
+(require 'dash)
+
+(setq result (let ((buffer (find-file-noselect "day01_input")))
+  (set-buffer buffer)
+  (-sum (-take 3 (-sort '> (-map
+	 (lambda (sublist) (-sum (-map 'string-to-number sublist)))
+	 (-split-on "" (split-string (buffer-string) "\n"))))))))
+(message (number-to-string result))
